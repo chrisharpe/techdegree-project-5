f55dd6a693028455d09691a634200b9d272873f9
chrisharpe <4chrisharpe@gmail.com>
2023-06-27 16:48:49 -0400

    creates /project/<id>/edit page

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html', projects=projects)
 
 
-@app.route('/projects/new', methods=['GET', 'POST'])
+@app.route('/project/new', methods=['GET', 'POST'])
 def new_project():
     projects = Project.query.all()
     if request.form:
@@ -25,29 +25,30 @@ def new_project():
     return render_template('projectform.html', projects=projects)
 
 
-@app.route('/projects/<id>')
+@app.route('/project/<id>')
 def detail(id):
     projects = Project.query.all()
     project = Project.query.get(id)
     return render_template('detail.html', projects=projects, project=project)
 
 
-@app.route('/edit/<id>', methods=['GET', 'POST'])
+@app.route('/project/<id>/edit', methods=['GET', 'POST'])
 def edit_project(id):
     project = Project.query.get(id)
     projects = Project.query.all()
+    formatted_date = project.date.strftime('%Y-%m')
     if request.form:
         project.title = request.form['title']
-        project.date = request.form['date']
+        project.date = clean_date(request.form['date'])
         project.description = request.form['description']
         project.skills = request.form['skills']
         project.repo_url = request.form['repo_url']
         db.session.commit()
-        return redirect(url_for('index.html'))
-    return render_template('projectform.html', project=project, projects=projects)
+        return redirect(url_for('index'))
+    return render_template('editproject.html', project=project, formatted_date=formatted_date, projects=projects)
 
 
-@app.route('/projects/<id>/delete')
+@app.route('/project/<id>/delete')
 def delete_project(id):
     project = Project.query.get(id)
     db.session.delete(project)
